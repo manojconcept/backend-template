ecf546c30a962dbce50dc2bc194c6af1a02fac97
manojconcept <manojveerapavu@gmail.com>
2025-02-12 20:42:34 +0530

    init

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -100,6 +100,7 @@ export const login = async (req, res) => {
                 // secure: process.env.NODE_ENV === 'production',  // Only send over HTTPS in production
                 secure:true,
                 sameSite: 'None',  // Prevents cross-site request forgery (CSRF)
+                domain: '.onrender.com',
                 maxAge: maxAge // Cookie expires in 7 days
             });
             await user.save()
