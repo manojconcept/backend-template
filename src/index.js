7f5f9227f3f6a1605af9e01807f363710c94d5a3
manojconcept <manojveerapavu@gmail.com>
2025-02-12 12:47:38 +0530

    added corsOptions

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const corsOptions = {
     credentials: true, // Allow cookies to be sent with the request
   };
   
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
