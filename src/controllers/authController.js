ec2c1e2a41ecd2f0c4d63740e321dcfdc23790ad
manojconcept <manojveerapavu@gmail.com>
2025-02-13 08:10:43 +0530

    init

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -96,13 +96,14 @@ export const login = async (req, res) => {
             );
             res.cookie('k-token', refreshToken, {
                 path: '/',
-                httpOnly: false,  // Cookie cannot be accessed via JavaScript
+                httpOnly: false, 
                 // secure: process.env.NODE_ENV === 'production',  // Only send over HTTPS in production
                 secure:false,
-                sameSite: 'None',  // Prevents cross-site request forgery (CSRF)
-                maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days, // Cookie expires in 7 days
-                partitioned: true, // Add 'Partitioned' (this depends on library support)
-                // expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+                sameSite: 'None',  
+                maxAge: 7 * 24 * 60 * 60 * 1000, 
+                partitioned: true,
+                domain: '.onrender.com',
+                
             });
             await user.save()
             return res.status(200).json({ accessToken, refreshToken });
