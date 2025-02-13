e7886a01cca6df6380f1fa764a62713b2f66b923
manojconcept <manojveerapavu@gmail.com>
2025-02-13 11:02:11 +0530

    init

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -19,6 +19,7 @@ router.use(multer().array());
 router.post('/signup',userAgent,signup);
 router.get('/verify/:token', verifyEmail);
 router.post('/login', login);
+router.get('/logout',verifyPasswordReset)
 router.post('/request-password-reset', requestPasswordReset); // pending...
 router.post('/reset-password', resetPassword); // pending...
 router.get('/refresh-token', refreshToken);
