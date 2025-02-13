7df8bdda7bc13a66be881ae9aa8af8108e3ccf73
manojconcept <manojveerapavu@gmail.com>
2025-02-13 12:21:27 +0530

    init

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -97,7 +97,7 @@ export const login = async (req, res) => {
             res.cookie('k-token', refreshToken, {
                 httpOnly: true,
                 secure: true,
-                sameSite: "Lex",
+                sameSite: "None",
                 maxAge: 24 * 60 * 60 * 1000,
             });
             await user.save()
